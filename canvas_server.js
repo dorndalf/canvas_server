a9e817c768381e7e8124603f7d7166535572a1a1
dorndalf <benedict.dorndorf@gmail.com>
2021-10-04 15:59:56 +0200

    Initial commit with basic server

---
+++ canvas_server.js
@@ -0,0 +1,26 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", function (socket3) {
+  io.engine === io.eio;
+  serverClients = Object.keys(io.engine.clients); //schreibt bei jedem join auf server die Verbundenen Sockets in ein Array
+});
+
+//---------------------------------------------------------- Textchat
+
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    io.emit("newMessage", message); //gibt aus dass wort richtig war
+    console.log(message);
+  });
+});
+
+http.listen(1333, function () {
+  console.log("listening on *:1333");
+});
